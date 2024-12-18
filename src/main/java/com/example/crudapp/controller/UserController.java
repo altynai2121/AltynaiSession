31e20c1d1c2a31f65f7fae295268c42ad39a45b9
A <altusha21072005@gmail.com>
2024-12-19 00:15:45 +0500

    Initial commit

---
+++ src/main/java/com/example/crudapp/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.crudapp.controller;
+
+import com.example.crudapp.model.User;
+import com.example.crudapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
---
+++ src/main/java/com/example/crudapp/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.crudapp.service;
+
+import com.example.crudapp.model.User;
+import com.example.crudapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
